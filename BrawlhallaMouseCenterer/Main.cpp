b39f3d473ce658111b96f404f84895f483c819da
Bobakanoosh <jack.bobakanoosh@gmail.com>
2022-12-15 23:39:14 -0600

    Clamp mouse in window instead enabling with hotkey

--- BrawlhallaMouseCenterer/Main.cpp
+++ BrawlhallaMouseCenterer/Main.cpp
@@ -2,46 +2,52 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <algorithm>
 
-#define WINDOW_CLASS "ApolloRuntimeContentWindow"
+constexpr auto WINDOW_CLASS = "ApolloRuntimeContentWindow";
+constexpr auto MOUSE_POSITION_BUFFER = 40;
+constexpr auto WINDOW_UPDATE_INTERVAL = std::chrono::milliseconds(100);
+constexpr auto WINDOW_SEEK_INTERVAL = std::chrono::seconds(5);
 
 int main() 
 {
-	bool enabled = false;
+	auto lastWindowTime = std::chrono::steady_clock::now();
+	HWND window = NULL;
+
+	printf("Cursor locked inside brawlhalla");
+
 	while (true) 
 	{
-		if (GetAsyncKeyState(VK_F5))
+		if (window != GetForegroundWindow())
 		{
-			enabled = !enabled;
-			printf("%s\n", enabled ? "enabled" : "disabled");
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
-		}
-
-		if (enabled)
-		{
-			HWND window = FindWindowA(WINDOW_CLASS, nullptr);
-			if (!window)
+			auto currentWindowTime = std::chrono::steady_clock::now();
+			if ((currentWindowTime - lastWindowTime).count() >= WINDOW_UPDATE_INTERVAL.count()) 
 			{
-				printf("Window not open\n");
-				std::this_thread::sleep_for(std::chrono::seconds(5));
-				continue;
+				lastWindowTime = currentWindowTime;
+				window = FindWindowA(WINDOW_CLASS, nullptr);
 			}
 
-			if (window != GetForegroundWindow())
-			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
-				continue;
-			}
-
-			RECT rect;
-			GetWindowRect(window, &rect);
-
-			const int mouseX = rect.left + 50;
-			const int mouseY = rect.top + 500;
-
-			SetCursorPos(mouseX, mouseY);
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			continue;
 		}
+
+		if (!window)
+		{
+			printf("Window not open\n");
+			std::this_thread::sleep_for(WINDOW_SEEK_INTERVAL);
+			continue;
+		}
+
+		RECT rect;
+		GetWindowRect(window, &rect);
+
+		POINT point;
+		GetCursorPos(&point);
+
+		const int mouseX = std::clamp(point.x, rect.left + MOUSE_POSITION_BUFFER, rect.right - MOUSE_POSITION_BUFFER);
+		const int mouseY = std::clamp(point.y, rect.top + MOUSE_POSITION_BUFFER, rect.bottom - MOUSE_POSITION_BUFFER);
+
+		SetCursorPos(mouseX, mouseY);
 	}
 
 	return 0;
